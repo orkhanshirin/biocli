6f2f7d580c4dcfcebf50af1af2737f7069f61a65
orkhanshirin <orkhan.shirinow@gmail.com>
2022-09-21 20:17:21 +0400

    refactor(develop): Modified changelog workflow
    - Modified changelog workflow to detect several extra words for proper labeling

--- src/biocli/__init__.py
+++ src/biocli/__init__.py
@@ -3,6 +3,6 @@ Computational Biology tool for CLI use.
 """
 from biocli import count, pattern, translate
 
-__version__ = "1.0.1b"
+__version__ = "1.0.1b1"
 
 __all__ = ["count", "pattern", "translate"]
